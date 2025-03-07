77f9eb88d9728e1d9012bfaeced705e8bdffd594
Badroommate <mccauley.andrew@icloud.com>
2025-03-07 15:05:14 -0500

    Update main_menu.py

--- main_menu.py
+++ main_menu.py
@@ -115,12 +115,12 @@ def options():
 
         SCREEN.fill("white")
 
-        OPTIONS_TEXT = get_font(45).render("This is the OPTIONS screen.", True, "Black")
+        OPTIONS_TEXT = get_font(45).render("OPTIONS", True, "Black")
         OPTIONS_RECT = OPTIONS_TEXT.get_rect(center=(width // 2, height // 4)) 
         SCREEN.blit(OPTIONS_TEXT, OPTIONS_RECT)
 
-        OPTIONS_BACK = Button(image=None, pos=(width // 2, height // 2), 
-                            text_input="BACK", font=get_font(75), base_color="Black", hovering_color="Green")
+        OPTIONS_BACK = Button(image=None, pos=(width // 2, height - 25), 
+                            text_input="BACK", font=get_font(30), base_color="Black", hovering_color="Green")
 
         OPTIONS_BACK.changeColor(OPTIONS_MOUSE_POS)
         OPTIONS_BACK.update(SCREEN)
